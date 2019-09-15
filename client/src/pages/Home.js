96e0ca23f54b192dcbfd1007ddd2d02d29c998f5
Yusuf Karim <ykarim250@gmail.com>
2019-09-15 10:07:29 -0400

    use bottstrap alert

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Badge, DropdownButton, NavDropdown, Button } from 'react-bootstrap';
+import { Badge, DropdownButton, NavDropdown, Button, Alert } from 'react-bootstrap';
 import BubbleChart from '../components/BubbleChart';
 import { withRouter } from 'react-router';
 
@@ -29,29 +29,29 @@ class Home extends Component {
   }
 
   onClickVoteHandler(postId, index) {
-    if(!this.state.voted[index]) {
-    fetch('http://localhost:5000/api/votePost', {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        postId: postId
-      }),
-    }).then(res => res.json())
-      .then(result => {
-        if (result && result.success) {
-          let temp = this.state.posts
-          temp[index].counter += 1
-          this.setState({posts: temp})
-        } else {
-          // TODO: show error to user
-        }
-      });
+    if (!this.state.voted[index]) {
+      fetch('http://localhost:5000/api/votePost', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          postId: postId
+        }),
+      }).then(res => res.json())
+        .then(result => {
+          if (result && result.success) {
+            let temp = this.state.posts
+            temp[index].counter += 1
+            this.setState({ posts: temp })
+          } else {
+            // TODO: show error to user
+          }
+        });
       let temp = this.state.voted;
       temp[index] = true
-      this.setState({voted: temp})
+      this.setState({ voted: temp })
       console.log(this.state.voted)
     }
   }
@@ -79,22 +79,22 @@ class Home extends Component {
   show(index) {
     let temp = this.state.hidden;
     temp[index] = !this.state.hidden[index]
-    this.setState({hidden: temp})
+    this.setState({ hidden: temp })
   }
 
   changeMode(mode) {
-    this.setState({fetchMode: mode})
+    this.setState({ fetchMode: mode })
     let temp = this.state.posts
     if (mode === 1) {
       console.log("sort by time")
-      temp.sort(function(a, b){return(moment(b.time).diff(moment(a.time)))})
+      temp.sort(function (a, b) { return (moment(b.time).diff(moment(a.time))) })
     } else {
       console.log("sort by count")
-      temp.sort(function(a, b){return(b.counter - a.counter)})
+      temp.sort(function (a, b) { return (b.counter - a.counter) })
     }
     var hidden = this.state.posts.map(x => true);
     var voted = this.state.posts.map(x => false);
-    this.setState({posts: temp, hidden: hidden, voted: voted})
+    this.setState({ posts: temp, hidden: hidden, voted: voted })
     console.log(temp)
   }
 
@@ -122,31 +122,31 @@ class Home extends Component {
     else {
       return (
         ((this.state.hidden[index]) ? (
-        <div style={{ flexDirection: "column", paddingBottom: "10px", paddingTop: "4px", borderBottom: "1px solid #8c8c8c", marginBottom: "10px", display: "flex", justifyContent: 'center', alignItems: 'center'}} key={index}>
-          <div>
-            This post may contain triggering content
-          </div>
-          <Button variant="link" onClick={() => this.show(index)}>Show post</Button>
-        </div>) :
-        (<div style={{ borderBottom: "1px solid #8c8c8c", marginBottom: "10px" }} key={index}>
-          <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
-            <div style={{ color: "#474747" }}>
-              {moment(post.time).fromNow()}
-            </div>
-            <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-              <Button variant="link" onClick={() => this.onClickVoteHandler(post._id, index)}>
-                I've Been There
-              </Button>
+          <div style={{ flexDirection: "column", paddingBottom: "10px", paddingTop: "4px", borderBottom: "1px solid #8c8c8c", marginBottom: "10px", display: "flex", justifyContent: 'center', alignItems: 'center' }} key={index}>
+            <Alert variant={'warning'}>
+              This post may contain triggering content
+            </Alert>
+            <Button variant="link" onClick={() => this.show(index)}>Show post</Button>
+          </div>) :
+          (<div style={{ borderBottom: "1px solid #8c8c8c", marginBottom: "10px" }} key={index}>
+            <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between" }}>
               <div style={{ color: "#474747" }}>
-                {post.counter}
+                {moment(post.time).fromNow()}
+              </div>
+              <div style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                <Button variant="link" onClick={() => this.onClickVoteHandler(post._id, index)}>
+                  I've Been There
+              </Button>
+                <div style={{ color: "#474747" }}>
+                  {post.counter}
+                </div>
               </div>
             </div>
-          </div>
-          {post.text}
-          <div className={"text-button"} style={{ color: "#474747", marginTop: "8px", marginBottom: "16px" }}>
-            {post.comments.length} comments
+            {post.text}
+            <div className={"text-button"} style={{ color: "#474747", marginTop: "8px", marginBottom: "16px" }}>
+              {post.comments.length} comments
         </div>
-        </div>))
+          </div>))
       )
     }
 
