130e8e80efe9c7f6769f46ab92ef0117d7c58b93
Johnny <jkairoy@gmail.com>
2019-09-24 15:35:45 -0400

    `build` added to source control

---
+++ client/build/precache-manifest.11ea3d83983c943a914d905498b9782f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0281e18fc4c5f976fa67",
+    "url": "/static/js/main.0281e18f.chunk.js"
+  },
+  {
+    "revision": "c25216666673ba8ec2b0",
+    "url": "/static/js/2.c2521666.chunk.js"
+  },
+  {
+    "revision": "0281e18fc4c5f976fa67",
+    "url": "/static/css/main.68ebfef0.chunk.css"
+  },
+  {
+    "revision": "c25216666673ba8ec2b0",
+    "url": "/static/css/2.e2153b0e.chunk.css"
+  },
+  {
+    "revision": "4c9e2d270c3041df3b1f6e8dc1d40028",
+    "url": "/index.html"
+  }
+];
